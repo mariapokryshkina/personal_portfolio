a66109423c32e0b240746419f8d9ab817f5882c2
Maria Pokryshkina <mariapokryshkina@Marias-Air.wlan>
2023-02-09 00:45:03 +0200

    updated navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
 		<nav className="nav">
 			<div className="container">
 				<div className="nav-row">
-					<NavLink to="/" className="logo"><strong>Freelancer</strong> portfolio</NavLink>
+					<NavLink to="/" className="logo"><strong>Personal</strong> portfolio</NavLink>
 
 					<BtnDarkMode />
 
