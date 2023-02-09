1694609e874c912f258d62b4607213ee5692b53f
Maria Pokryshkina <mariapokryshkina@Marias-Air.wlan>
2023-02-10 00:36:11 +0200

    github links updated

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -29,7 +29,7 @@ const Project = () => {
                 </div>
 
 
-                <BtnGitHub link="https://github.com/mariapokryshkina" />
+                <BtnGitHub link={project.gitHubLink} />
 
 
 
